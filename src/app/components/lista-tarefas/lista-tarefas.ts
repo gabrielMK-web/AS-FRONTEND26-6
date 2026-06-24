06873dae897accfe53285ef96649187b2e0ba16f
Gabriel Marin <gabriel.mkafer@rede.ulbra.br>
2026-06-23 21:39:52 -0300

    modelagem do componente lista-tarefas

--- src/app/components/lista-tarefas/lista-tarefas.ts
+++ src/app/components/lista-tarefas/lista-tarefas.ts
@@ -2,8 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lista-tarefas',
-  imports: [],
+  standalone: true,
   templateUrl: './lista-tarefas.html',
   styleUrl: './lista-tarefas.css',
 })
-export class ListaTarefas {}
+export class ListaTarefas {
+
+  tarefas = [ // lista temporaria
+    {
+      titulo: 'Exemplo de tarefa',
+      descricao: 'Isso é uma tarefa teste',
+      concluida: false
+    }
+  ];
+
+  constructor() {
+    
+  }}
+
+
+
+
