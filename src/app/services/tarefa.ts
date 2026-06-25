73f6ee22a9cfbec5e389b863cd7511c0a74891d3
Gabriel Marin <gabriel.mkafer@rede.ulbra.br>
2026-06-25 17:36:45 -0300

    criação do service com o array

--- src/app/services/tarefa.ts
+++ src/app/services/tarefa.ts
@@ -3,4 +3,19 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class Tarefa {}
+export class Tarefa {
+  private tarefas: any[] = []; // esse array armazena todas as tarefas do sistema
+
+  constructor() {}
+
+  
+  adicionarTarefa(tarefa: any): void { // adiciona uma tarefa no array
+    this.tarefas.push(tarefa);
+  }
+
+ 
+  listarTarefas(): any[] { //retrona as tarefas que foram cadastradas 
+    return this.tarefas;
+  }
+
+}
