587be1f96784f9c4571e4df36349f015a981f7c7
Gabriel Marin <gabriel.mkafer@rede.ulbra.br>
2026-06-25 15:27:23 -0300

    terminando de comentar o lista-tarefas

--- src/app/components/lista-tarefas/lista-tarefas.ts
+++ src/app/components/lista-tarefas/lista-tarefas.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';//decorador do angular
 
 @Component({
-  selector: 'app-lista-tarefas',
-  standalone: true,
-  templateUrl: './lista-tarefas.html',
-  styleUrl: './lista-tarefas.css',
+  selector: 'app-lista-tarefas', // Nome da tag html
+  standalone: true, // Componente independente (Angular novo)
+  templateUrl: './lista-tarefas.html', //HTML do componente
+  styleUrl: './lista-tarefas.css', //Css
 })
 export class ListaTarefas {
 
